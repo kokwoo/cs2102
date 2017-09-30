24c4245682139568ac50b22434bf6123fe6eda40
kokwoo <wongkokwoo@gmail.com>
2017-10-01 01:51:37 +0800

    adding logic and borrow item php

--- borrowitems.php
+++ borrowitems.php
@@ -1,5 +1,7 @@
 <?php
+define('included', true);
 include 'includes/HtmlUtilities.php';
+include 'logic/itemListController.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,46 +16,15 @@ include 'includes/HtmlUtilities.php';
   </head>
 
 
+
+
   <body>
     <?php HtmlUtilities::printHeader(); ?>
 
     <div class="container">
       <h1 class="display-4 text-center">Items Listing</h1>
       <hr>
-
-        <div class="card-deck">
-          <div class="card">
-            <img class="card-img-top" src="itemimages/image59cfaa596f675.png" alt="Card image cap">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-          <div class="card">
-            <img class="card-img-top" src="itemimages/image59cfaa596f675.png" alt="Card image cap">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-          <div class="card">
-            <img class="card-img-top" src="itemimages/image59cfaa596f675.png" alt="Card image cap">
-            <div class="card-body">
-              <h4 class="card-title">Card title</h4>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-      </div>
-    <br>
+        <?php itemListController::showListing(); ?>
     </div>
 
     <?php HtmlUtilities::printFooter(); ?>
---
+++ logic/itemListController.php
@@ -0,0 +1,77 @@
+<?php
+
+if (!defined('included')) {
+    include 'AppSession.php';
+    include '../DbConnection.php';
+    include '../includes/GlobalConstants.php';
+} else {
+    include 'DbConnection.php';
+    include 'includes/GlobalConstants.php';
+
+}
+
+class itemListController {
+
+    public static function showListing() {
+
+        $user = AppSession::getCurrentUser();
+        $db = DbConnection::getInstance();
+
+
+        $result = $db -> executeQuerySafe("SELECT * FROM itemimages, items WHERE itemimages.itemid = items.itemid;");
+
+        while ($row = pg_fetch_assoc($result)) {
+          print '<div class="card-deck">';
+          // for ($x = 0; $x < 3; $x++) {
+           print '<div class="card">';
+           print '<img class="card-img-top" src="itemimages/' . $row['imagename'] . '" alt="' . $row['name'] . '">';
+           print '<div class="card-body">';
+           print '<h4 class="card-title">' . $row['name'] . '</h4>';
+           print '<p class="card-text">' . $row['description'] . '</p>';
+           print '</div>';
+           print '<div class="card-footer">';
+           print '<small class="text-muted">Last updated 3 mins ago</small>';
+           print '</div></div>';
+
+          // }
+            print '</div><br>';
+        }
+
+    }
+}
+?>
+
+
+<!-- <div class="card-deck">
+    <div class="card">
+      <img class="card-img-top" src="itemimages/image59cfaa596f675.png" alt="Card image cap">
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="itemimages/image59cfaa596f675.png" alt="Card image cap">
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="itemimages/image59cfaa596f675.png" alt="Card image cap">
+      <div class="card-body">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+</div>
+<br> -->
