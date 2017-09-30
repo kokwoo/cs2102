edad23445d3d7dc08b65665438e7a68d5b9fe387
MightyCupcakes <gohydj2003@gmail.com>
2017-09-30 13:14:29 +0800

    Add type data

---
+++ sql scripts/type_data.sql
@@ -0,0 +1,41 @@
+INSERT INTO types VALUES('Furniture', NULL);
+INSERT INTO types VALUES('Chairs', 'Furniture');
+INSERT INTO types VALUES('Tables', 'Furniture');
+INSERT INTO types VALUES('Other Furniture', 'Furniture');
+
+INSERT INTO types VALUES('Home Appliances', NULL);
+INSERT INTO types VALUES('Kitchen Appliances', 'Home Appliances');
+INSERT INTO types VALUES('Household Appliances', 'Home Appliances');
+INSERT INTO types VALUES('Cleaning Appliances', 'Home Appliances');
+INSERT INTO types VALUES('Other Appliances', 'Home Appliances');
+
+INSERT INTO types VALUES('Home Hardware', NULL);
+INSERT INTO types VALUES('Drills', 'Home Hardware');
+INSERT INTO types VALUES('Screwdrivers', 'Home Hardware');
+INSERT INTO types VALUES('Hex keys', 'Home Hardware');
+INSERT INTO types VALUES('Gardening tools', 'Home Hardware');
+INSERT INTO types VALUES('Other Hardware', 'Home Hardware');
+
+INSERT INTO types VALUES('Movies and games', NULL);
+INSERT INTO types VALUES('DVD Movies', 'Movies and games');
+INSERT INTO types VALUES('Blu-Ray Movies', 'Movies and games');
+INSERT INTO types VALUES('CD Movies', 'Movies and games');
+INSERT INTO types VALUES('CD Games', 'Movies and games');
+INSERT INTO types VALUES('DVD Games', 'Movies and games');
+INSERT INTO types VALUES('Digital key games', 'Movies and games');
+INSERT INTO types VALUES('Other Movies', 'Movies and games');
+INSERT INTO types VALUES('Other Games', 'Movies and games');
+
+
+INSERT INTO types VALUES('Books', NULL);
+INSERT INTO types VALUES('Self-improvement books', 'Books');
+INSERT INTO types VALUES('General reading books', 'Books');
+INSERT INTO types VALUES('Textbooks', 'Books');
+INSERT INTO types VALUES('e-books', 'Books');
+INSERT INTO types VALUES('Other Books', 'Books');
+
+INSERT INTO types VALUES('Home Office', NULL);
+INSERT INTO types VALUES('Stationery', 'Home Office');
+INSERT INTO types VALUES('Printers', 'Home Office');
+INSERT INTO types VALUES('Scanners', 'Home Office');
+INSERT INTO types VALUES('Others', 'Home Office');
