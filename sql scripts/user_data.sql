e587955c01c7402acd2e7c08340d37861356bf82
MightyCupcakes <gohydj2003@gmail.com>
2017-10-01 23:35:17 +0800

    Add plaintext passwords for users as sql comments

--- sql scripts/user_data.sql
+++ sql scripts/user_data.sql
@@ -1,22 +1,42 @@
+-- Password: password
 INSERT INTO users VALUES ('rulesincere', 'Louetta Pattee', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', '205A Pandan Loop 128442, Singapore', 'users', now());
+-- Password: password1
 INSERT INTO users VALUES ('omniscientsuggest', 'Lamont Eusebio', '0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e', '20 Havelock Road #01-00 Central Square, 059765, Singapore', 'users', now());
+-- Password: password2
 INSERT INTO users VALUES ('lizardburberry', 'Joaquina Copeland', '6cf615d5bcaac778352a8f1f3360d23f02f34ec182e259897fd6ce485d7870d4', '75 Science Park #01-03 Cintech II, 118255, Singapore', 'users', now());
+-- Password: password3
 INSERT INTO users VALUES ('mildjackrabbit', 'Ayako Breece', '5906ac361a137e2d286465cd6588ebb5ac3f5ae955001100bc41577c3d751764', 'Queenstown Lutheran Church 709 Commonwealth Drive Singapore 149601, Singapore', 'users', now());
+-- Password: password4
 INSERT INTO users VALUES ('adddeveloper', 'Verna Gladfelter' , 'b97873a40f73abedd8d685a7cd5e5f85e4a9cfb83eac26886640a0813850122b', '3 Temasek Boulevard #02-048 Suntec City Mall, 038983, Singapore', 'users', now());
+-- Password: password5
 INSERT INTO users VALUES ('costaricanforgive', 'Anneliese Estey', '8b2c86ea9cf2ea4eb517fd1e06b74f399e7fec0fef92e3b482a6cf2e2b092023', '1 Changi South Street 2 Level 5, 486760, Singapore', 'users', now());
+-- Password: password6
 INSERT INTO users VALUES ('writesteady', 'Rosette Haverly', '598a1a400c1dfdf36974e69d7e1bc98593f2e15015eed8e9b7e47a83b31693d5', 'Great World City 1 Kim Seng Promenade #15-07/#15-11 237994, Singapore', 'users', now());
+-- Password: password7
 INSERT INTO users VALUES ('cockytrouble', 'Davina Goya', '5860836e8f13fc9837539a597d4086bfc0299e54ad92148d54538b5c3feefb7c', '26 Clementi Loop #01-55 Clementi Camp, 129817, Singapore', 'users', now());
+-- Password: password8
 INSERT INTO users VALUES ('lutzsecretive', 'Jacqulyn Buchannon', '57f3ebab63f156fd8f776ba645a55d96360a15eeffc8b0e4afe4c05fa88219aa', 'Block 7 Pioneer Road North #01-51 Singapore 628459, Singapore', 'users', now());
+-- Password: password9
 INSERT INTO users VALUES ('forgetfulfix', 'Jill Uhlig', '9323dd6786ebcbf3ac87357cc78ba1abfda6cf5e55cd01097b90d4a286cac90e', '511 Kampong Bahru Road #03-02 Keppel Distripark, 099447, Singapore', 'users', now());
+-- Password: password10
 INSERT INTO users VALUES ('miniaturestupid', 'Jennefer Volpe', 'aa4a9ea03fcac15b5fc63c949ac34e7b0fd17906716ac3b8e58c599cdc5a52f0', '8 Neo Pee Teck Lane, 119034, Singapore', 'users', now());
+-- Password: password11
 INSERT INTO users VALUES ('tamarinkerching', 'Elias Harbor', '53d453b0c08b6b38ae91515dc88d25fbecdd1d6001f022419629df844f8ba433', '36 Ayer Rajah Crescent #03-00, 139945, Singapore', 'users', now());
+-- Password: password12
 INSERT INTO users VALUES ('decisiveactor', 'Nam Tekulve', 'b3d17ebbe4f2b75d27b6309cfaae1487b667301a73951e7d523a039cd2dfe110', 'Banyan tree building 211 upper bukit timah road, 588182, Singapore', 'users', now());
+-- Password: password13
 INSERT INTO users VALUES ('obeisantunsuitable', 'Terrilyn Holliman', '48caafb68583936afd0d78a7bfd7046d2492fad94f3c485915f74bb60128620d', '1 Hougang Street 91 #01-41 HOUGANG FESTIVAL MARKET, 538692, Singapore', 'users', now());
+-- Password: password14
 INSERT INTO users VALUES ('foxbelated', 'Kindra Hockensmith', 'c6863e1db9b396ed31a36988639513a1c73a065fab83681f4b77adb648fac3d6', '23 Defu Lane 4, 539421, Singapore', 'users', now());
+-- Password: password15
 INSERT INTO users VALUES ('cattlebounce', 'Marilynn Neri', 'c63c2d34ebe84032ad47b87af194fedd17dacf8222b2ea7f4ebfee3dd6db2dfb', 'Block 77, Marine Drive, ,06-48 440077, Singapore', 'users', now());
+-- Password: password16
 INSERT INTO users VALUES ('racingoblongata', 'Deangelo Peake', '17a3379984b560dc311bb921b7a46b28aa5cb495667382f887a44a7fdbca7a7a', 'Blk 5000, Ang Mo Kio Ave 5 Techplace II, 03-04 569870, Singapore', 'users', now());
+-- Password: password17
 INSERT INTO users VALUES ('agitatedlabored', 'Danuta Loughran', '69bfb918de05145fba9dcee9688dfb23f6115845885e48fa39945eebb99d8527', 'Shanker Tekri Udoygnagar, 478963 Jamnagar, Singapore', 'users', now());
+-- Password: password18
 INSERT INTO users VALUES ('subalternovercooked', 'Clarissa Poteat', 'd2042d75a67922194c045da2600e1c92ff6d87e8fb6e0208606665f2d1dfa892', '445 Clementi Ave 3 #01-173, 120445, Singapore', 'users', now());
+-- Password: password19
 INSERT INTO users VALUES ('boorishinvincible', 'Alise Squires', '5790ac3d0b8ae8afc72c2c6fb97654f2b73651c328de0a3b74854ade562dd17a', '22 Sin Ming Lane,16-76 Midview City Singapore 573969, Singapore', 'users', now());
 
 -- Super Admin account password: 1
